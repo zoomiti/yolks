86a97cf3626fd192078d7082a337263464362611
Softwarenoob <admin@softwarenoob.com>
2021-08-22 04:44:01 +0300

    feat: migrate all java images from alpine to debian

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/ash
+#!/bin/bash
 
 #
 # Copyright (c) 2021 Matthew Penner
@@ -20,7 +20,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-# 
+#
 
 # Default the TZ environment variable to UTC.
 TZ=${TZ:-UTC}
