c0daa84da87bb569ecf55569446c3fdd7a012ebc
QuintenQVD0 <josdekurk@gmail.com>
2022-11-06 17:51:12 +0100

    Dotnet arm64 fix

--- dotnet/entrypoint.sh
+++ dotnet/entrypoint.sh
@@ -5,6 +5,9 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+# set this variable, dotnet needs it even without it it reports to `dotnet --info` it can not start any aplication without this
+export DOTNET_ROOT=/usr/share/
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
