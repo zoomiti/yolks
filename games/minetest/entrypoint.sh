4a8781c4679a19bca752c0e00dd7fd683becf521
Quinten <67589015+QuintenQVD0@users.noreply.github.com>
2023-02-16 16:20:26 +0100

    games: add minetest

---
+++ games/minetest/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+# Print minetest version
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mminetest --version\n"
+minetest --version
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
